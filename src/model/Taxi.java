2fb59f20e5ee1c077425106e20be8795f75f1109
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-08-03 11:00:38 +0700

    update package

---
+++ src/model/Taxi.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.io.Serializable;
+
+public class Taxi extends Vehicle implements Serializable {
+    public static final int PRICE_PER_1_KM = 10;
+
+    public Taxi(String licensePlate, String driverName) {
+        super(licensePlate, driverName);
+    }
+
+    @Override
+    public int getRevenue() {
+        return this.getDistance()*PRICE_PER_1_KM;
+    }
+}
